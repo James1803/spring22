29d976e89c8eb86006de21eeb88e75f88a2f7177
MrWalshyType2 <mwalsh@qa.com>
2022-04-21 22:52:27 +0100

    Added schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+create table post (id integer not null auto_increment, content varchar(255) not null, posted_at date not null, title varchar(32) not null, user_id integer, primary key (id));
+
+create table user (id integer not null auto_increment, email varchar(255) not null, username varchar(16) not null, primary key (id));
+
+alter table post add constraint FK72mt33dhhs48hf9gcqrq4fxte foreign key (user_id) references user (id);
